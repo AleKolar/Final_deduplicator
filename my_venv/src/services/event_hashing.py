fd38710dc44f8c7b319c78455d2d1d47787f5d0a
AleKolar <157892507+AleKolar@users.noreply.github.com>
2025-04-20 17:14:33 +0300

    added_event_hashing

--- my_venv/src/services/event_hashing.py
+++ my_venv/src/services/event_hashing.py
@@ -6,7 +6,7 @@ from my_venv.src.services.normalize import normalize_for_hashing
 
 
 def generate_event_hash(raw_data: Dict[str, Any], event_name: str) -> str:
-    """Генерирует SHA-256 хэш от нормализованных данных."""
+    """Генерирует SHA-256 хэш от нормализованных данных """
     normalized_data = normalize_for_hashing(raw_data, event_name)
     return hashlib.sha256(
         orjson.dumps(
@@ -16,5 +16,5 @@ def generate_event_hash(raw_data: Dict[str, Any], event_name: str) -> str:
     ).hexdigest()
 
 def verify_event_hash(event_hash: str, raw_data: Dict, event_name: str) -> bool:
-    """Проверяет, соответствует ли хэш текущим данным."""
+    """Проверяет, соответствует ли хэш текущим данным """
     return event_hash == generate_event_hash(raw_data, event_name)
